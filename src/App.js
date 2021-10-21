c3d63a2affb6a2fa542531311c4af2e296530516
ciarasugrue <86407704+ciarasugrue@users.noreply.github.com>
2021-10-21 13:30:19 +0200

    Setup Dictionary Folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Dictionary by Ciara
+      </header>
+    </div>
+  );
+}
+
+export default App;
