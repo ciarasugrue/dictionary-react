c086423446e65f6ef93e2bdc6a5ec0b12bd74486
ciarasugrue <86407704+ciarasugrue@users.noreply.github.com>
2021-10-26 14:17:27 +0200

    Created Dictionary search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">Dictionary by Ciara
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>Coded by Ciara</footer>
+      </div>
     </div>
   );
 }
