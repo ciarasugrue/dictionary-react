53e67c1a2075304defcefecc7475d4393a3e2ecc
ciarasugrue <86407704+ciarasugrue@users.noreply.github.com>
2022-01-25 19:21:39 +0100

    Added styling

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Phonetic.css';
 
 export default function Phonetic(props) {
  console.log(props.phonetic);
  return (
-<div className="phonetics" >
-    <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+<div className="phonetics">
+    <a  className="audio" href={props.phonetic.audio} target="_blank" rel="noreferrer">
     Listen
     </a>
     <br />
